e6e985cb196822c5606586413d6ffc391404406c
saurabh chuadhary <supersaurabh017@gmail.com>
2022-09-05 02:38:52 +0530

    Memes-generator
    Its is a website which you can use to create funny memes and download it locally to share it with your friends

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
+import Tempelates from "./components/Tempelates";
+import Meme from "./components/Meme";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [tempelates, setTempelates] = useState([]);
+  const [meme, setMeme] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  async function fetchAPI() {
+    var res = await fetch("https://api.imgflip.com/get_memes");
+    var data = await res.json();
+    console.log(data.data.memes);
+    setTempelates(data.data.memes);
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <div>
+      <h1>Meme Generator App</h1>
+      {meme === null ? (
+        <Tempelates tempelates={tempelates} setMeme={setMeme} />
+      ) : (
+        <Meme meme={meme} setMeme={setMeme} />
+      )}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
